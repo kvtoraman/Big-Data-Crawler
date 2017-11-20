77c1f36eb5291906e58a44571b5510ea95e86703
themis0888 <themis0888@gmail.com>
2017-11-20 20:53:18 +0900

    last missing song list

--- nrb_crawler.py
+++ nrb_crawler.py
@@ -5,8 +5,8 @@ from urllib import parse
 Num = 23
 
 
-def NRBcrwal(name,fname):
-    targetUrl = "https://m.search.naver.com/search.naver?where=m&query="+parse.quote(name)
+def NRBcrwal(name,fname,i,fee):
+    targetUrl = "https://m.search.naver.com/search.naver?where=m&query="+parse.quote(name+'+가사')
     targetRequest = Request(targetUrl)
     response = urlopen(targetRequest)
     responseText = response.read()
@@ -15,7 +15,9 @@ def NRBcrwal(name,fname):
     targetList = soup.select('#_cs_music_track > div.group_music._tab_panel > div.lyrics_area._panel._hp_style._hp_class > div.lyrics_txt')
     #print(targetList)
     if targetList == []:
-        print('Error line: ' + name)
+        
+        fee.write(name+'\n')
+        print('Error line: ' + name+'\n')
     for i,elem in enumerate(targetList):
         fname.write(elem.text)
         fname.write('\n')
@@ -27,9 +29,10 @@ for i in range(Num):
     strw = 'noraebang_data/nrb_lyrics' + str(i) + '.txt'
     fw = open(strw,'w',encoding='UTF8')
     line = fr.read().split('\n')
+    fee = open('noraebang_data/Error_Song'+str(i)+'.txt','w',encoding='UTF8')
     
     
-    for lst in line:
-        NRBcrwal(lst+'+가사', fw)
+    for j in range(len(line)):
+        NRBcrwal(line[j], fw, j, fee)
 
 #NRBcrwal('윤종신+좋니')
